6c9d782fcf16ae37f2c303779f6a5bbf6c6e5f0e
gokulraj3009 <118839793+gokulraj3009@users.noreply.github.com>
2022-11-27 19:26:49 +0530

    Create string_methods2.java

---
+++ JAVA/string_methods2.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+	   String s1 = "Hands On Practise";
+	   String s2 = "   For Virtusa";
+	   String s3 = "testing";
+	   String s4 = "testing";
+	   
+	   System.out.println(s1.charAt(3));
+	   System.out.println(s3.compareTo(s4));
+	   System.out.println(s1.compareTo(s3));
+	   System.out.println(s2.concat(s1));
+	   System.out.println(s3.contains(s4));
+	   System.out.println(s1.equals(s3));
+	   System.out.println(s1.length());
+	   System.out.println(s1.indexOf("d"));
+	}
+}
