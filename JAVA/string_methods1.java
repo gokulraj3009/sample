4071d52a810cd0c7c6055d82efa72a056bd0f04d
gokulraj3009 <118839793+gokulraj3009@users.noreply.github.com>
2022-11-27 19:04:42 +0530

    Create string_methods1.java

---
+++ JAVA/string_methods1.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+	   String s1 = "Hands On Practise";
+	   String s2 = "   For Virtusa";
+	   System.out.println(s1.toUpperCase());
+	   System.out.println(s2.toLowerCase());
+	   System.out.println(s2.trim());
+	}
+}
