75b3177ac02caa3b0c12fefb592da89d0a45d2fa
gokulraj3009 <118839793+gokulraj3009@users.noreply.github.com>
2022-11-27 18:41:47 +0530

    Create ifelse.java

---
+++ JAVA/ifelse.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+	   int mark = 30;
+	   
+	   if(mark>80)
+	   System.out.print("good");
+	   else if(mark<35)
+	   System.out.print("average");
+	   else
+	   System.out.print("fail");
+	    
+	}
+}
